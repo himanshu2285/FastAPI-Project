bf6a9d4de7f643da4617661ab481fc948489c3d6
himanshu2285 <himanshu2k05m@gmail.com>
2026-06-16 13:59:20 +0530

    Enhance patient management API with dynamic patient retrieval and path parameter validation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 import json
 
 app = FastAPI()
@@ -18,7 +18,20 @@ def about():
     return {"message": "A fully functional API for managing patient records"}
 
 
-@app.get("/view")
-def view():
+@app.get("/patient")
+def view_patients():
     data = load_data()
     return {"patients": data}
+
+
+# Path Params: Path parameters are dynamic segments of a URL path used to identify a specific resources.
+# Example: /patients/{patient_id} where {patient_id} is a path parameter that can be replaced with an actual patient ID to retrieve specific patient information.
+
+# Path(): to provide additional validation and metadata for path parameters.
+
+@app.get("/patient/{patient_id}")
+def get_patient(patient_id: str):
+    data = load_data()
+    if patient_id in data:
+        return {"patient": data[patient_id]}
+    return {"error": "Patient not found"}
