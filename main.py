32b402f77630f3da13bb8a274d3f0d945af03989
himanshu2285 <himanshu2k05m@gmail.com>
2026-06-16 15:43:49 +0530

    Path & Query Parameter: Enhance patient retrieval API with error handling and sorting functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, HTTPException, Path, Query
 import json
 
 app = FastAPI()
@@ -30,8 +30,28 @@ def view_patients():
 # Path(): to provide additional validation and metadata for path parameters.
 
 @app.get("/patient/{patient_id}")
-def get_patient(patient_id: str):
+def get_patient(patient_id: str = Path(..., description="The ID of the patient in DB", example="P001")):
     data = load_data()
     if patient_id in data:
         return {"patient": data[patient_id]}
-    return {"error": "Patient not found"}
+    # return {"error": "Patient not found"}
+    raise HTTPException(status_code=404, detail="Patient not found")
+
+
+@app.get('/sort')
+def sort_patient(sort_by: str = Query(..., description='Sort on the basis of Height, Weight and BMI'),
+                order: str = Query('asc', description='sort on asc or desc order')):
+    valid_fields = ['height','weight','bmi']
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f'Invalid field, select from {valid_fields}')
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail=f'Invalid order select asc or desc order')
+    
+    data = load_data()
+    
+    sort_order = True if order=='desc' else False
+    
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by,0), reverse=sort_order)
+    
+    return sorted_data
